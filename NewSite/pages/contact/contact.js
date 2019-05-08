574dbdbc60a0c6e4de082d06ff6d1bba27ae0c1a
mrt1209 <mrt1209@gmail.com>
2019-05-07 22:18:30 -0400

    significantly changing how I'm sending the formspree email. No longer using ajax call.

--- NewSite/pages/contact/contact.js
+++ NewSite/pages/contact/contact.js
@@ -7,61 +7,29 @@
             controller: contactController
         });
 
-    contactController.inject = ['$http'];
-
-    function contactController($http) {
+    function contactController() {
         var contactCtrl = this;
 
-        var oakhillEmail = "matt@oakhill.media";
-        var email_subject = "New Inquiry!";
-        var success_message = "Your message was sent! We'll send over our packages and options soon!";
-        var error_message = "Oops. There was an error when trying to send your message. \n " +
-            "Please email us directly at matt@oakhill.media and we'll get back to you as soon as we can. Sorry for the inconvenience!";
+        contactCtrl.submit = submit;
+        contactCtrl.updateBody = updateBody;
 
         function init() {
             contactCtrl.message = "test message ";
             contactCtrl.contactform = {};
             contactCtrl.showMessage = true;
-            contactCtrl.contactform.body = "Date: " + contactCtrl.contactform.date + "\n" +
-                "Venue: " + contactCtrl.contactform.venue + "\n" +
-                "Fiance: " + contactCtrl.contactform.fiance + "\n" +
-                "About the couple: " + contactCtrl.contactform.aboutCouple
-
-            contactCtrl.submit = submit;
         }
 
         init();
 
         function submit() {
-            $http({
-                url: "http://formspree.io/" + oakhillEmail,
-                data: {
-                    name: contactCtrl.contactform.name,
-                    email: contactCtrl.contactform.email,
-                    comments: contactCtrl.contactform.body,
-                    test: "Hey",
-                    _subject: email_subject,
-                },
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                transformRequest: function (obj) {
-                    var str = [];
-                    for (var p in obj)
-                        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-                    return str.join("&");
-                }
-            }).then(function (response) {
-                contactCtrl.message = success_message;
-                contactCtrl.messageType = "success";
+            console.log("attempting to send this message: \n" + contactCtrl.contactform.body);
+        }
 
-            }).catch(function (error) {
-                contactCtrl.message = error_message;
-                console.log("error sending... \n" + "error message: " + error.data.error + "\n" + "error status: " + error.status);
-                contactCtrl.messageType = "error";
-            });
+        function updateBody() {
+            contactCtrl.contactform.body = "Date: " + contactCtrl.contactform.date + "\n" +
+                "Venue: " + contactCtrl.contactform.venue + "\n" +
+                "Fiance: " + contactCtrl.contactform.fiance + "\n" +
+                "About the couple: " + contactCtrl.contactform.aboutCouple;
         }
     }
 
